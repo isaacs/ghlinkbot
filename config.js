fd5c4496867c12799e34fd07378afd2b5db442b2
isaacs <i@izs.me>
2013-08-02 17:27:38 -0700

    config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+exports.server = 'irc.freenode.net'
+exports.port = 6667
+exports.nick = 'GH9'
+// { <room>: <default project> }
+exports.rooms = {
+  libuv: 'joyent/node',
+  'node.js': 'joyent/node'
+}
